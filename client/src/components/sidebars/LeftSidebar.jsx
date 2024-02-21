7e3e09a40e127588f83876fe2a3c3b2b9755ac93
Ariel Lin <72408793+ariellinfy@users.noreply.github.com>
2024-02-21 13:33:03 -0700

    Changed variable name and code formatting

--- client/src/components/sidebars/LeftSidebar.jsx
+++ client/src/components/sidebars/LeftSidebar.jsx
@@ -3,20 +3,21 @@ import CreateRoom from "../popups/CreateRoom";
 import UserSettings from "../popups/UserSettings";
 import Room from "../member/Room";
 
-
 const LeftSidebar = (props) => {
-    return (
+  return (
     <div className="flex flex-col justify-between w-1/5 min-w-fit h-full">
-        <h1 className="text-lg font-bold mt-4 text-center">Available Rooms</h1>
-        <div className="p-4 text-gray-800 h-[70%] overflow-y-hidden">
-
-                {/* Scrollable Container for Room Components */}
-                <div id="roomContainer" className="overflow-y-auto flex flex-col items-center items-stretch space-y-3 h-full">
-                  {/* Example for how Room components are used */}
-                    <Room name="CPSC 559 Study Group" link="/chatroom"/>
-                    <Room name="413 Study Pals"/>
-                    <Room name="Gaming Discussion"/>
-                    {/* <Room/>
+      <h1 className="text-lg font-bold mt-4 text-center">Available Rooms</h1>
+      <div className="p-4 text-gray-800 h-[70%] overflow-y-hidden">
+        {/* Scrollable Container for Room Components */}
+        <div
+          id="roomContainer"
+          className="overflow-y-auto flex flex-col items-center items-stretch space-y-3 h-full"
+        >
+          {/* Example for how Room components are used */}
+          <Room name="CPSC 559 Study Group" link="/chatroom" />
+          <Room name="413 Study Pals" />
+          <Room name="Gaming Discussion" />
+          {/* <Room/>
                     <Room/>
                     <Room/>
                     <Room/>
@@ -37,28 +38,25 @@ const LeftSidebar = (props) => {
                     <Room/>
                     <Room name="Last Room 1"/>
                     <Room name="Last Room 2"/> */}
-                    
-                    
-                  
-                </div>
-          </div>
-          <div className="p-4 flex flex-col justify-between">
-            <hr className="my-6 border-gray-200 dark:border-gray-400" />
-              
-            {/* Button for User Settings */}
-            <UserSettings />
-            
-            <hr className="my-6 border-gray-200 dark:border-gray-400" />
-            
-            {/* Container for Join Room and Create Room buttons */}
-            <div className="flex flex-col gap-4">
-                {/* Buttons for Joining and Creating a Room */}
-                <JoinRoom />
-                <CreateRoom />
-            </div>
-          </div>
+        </div>
       </div>
-    );
+      <div className="p-4 flex flex-col justify-between">
+        <hr className="my-6 border-gray-200 dark:border-gray-400" />
+
+        {/* Button for User Settings */}
+        <UserSettings />
+
+        <hr className="my-6 border-gray-200 dark:border-gray-400" />
+
+        {/* Container for Join Room and Create Room buttons */}
+        <div className="flex flex-col gap-4">
+          {/* Buttons for Joining and Creating a Room */}
+          <JoinRoom />
+          <CreateRoom />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftSidebar;
--- client/src/components/sidebars/RightSidebar.jsx
+++ client/src/components/sidebars/RightSidebar.jsx
@@ -2,20 +2,21 @@ import RoomSettings from "../popups/RoomSettings";
 import ShareRoom from "../popups/ShareRoom";
 import Member from "../member/Member";
 
-
 const RightSidebar = (props) => {
-    return (
+  return (
     <div className="flex flex-col justify-between h-full w-1/5 min-w-fit">
-        <h1 className="text-lg mt-4 text-center">Current Members</h1>
-        <div className="p-4 text-gray-900 h-[74%] overflow-y-hidden">
-              
-              {/* Scrollable Container for People Components */}
-              <div id="memberContainer" className="overflow-y-auto flex flex-col items-center items-stretch space-y-3 h-full">
-                  {/* Example member components. Members should be dynamically added from the backend */}
-                  <Member name="You" isOwner="true"/>
-                  <Member name="Jennifer Lawrence" isOwner="false"/>
-                  <Member isAFK="true"/>
-                  {/* <Member name="Andrew 'Spider-Man' Garfield" isOwner="false"/>
+      <h1 className="text-lg mt-4 text-center">Current Members</h1>
+      <div className="p-4 text-gray-900 h-[74%] overflow-y-hidden">
+        {/* Scrollable Container for People Components */}
+        <div
+          id="memberContainer"
+          className="overflow-y-auto flex flex-col items-center items-stretch space-y-3 h-full"
+        >
+          {/* Example member components. Members should be dynamically added from the backend */}
+          <Member name="You" isOwner="true" />
+          <Member name="Jennifer Lawrence" isOwner="false" />
+          <Member isAFK="true" />
+          {/* <Member name="Andrew 'Spider-Man' Garfield" isOwner="false"/>
                   <Member />
                   <Member />
                   <Member />
@@ -43,22 +44,22 @@ const RightSidebar = (props) => {
                   <Member />
                   <Member name="Last Dude 1"/>
                   <Member name="Last Dude 2"/> */}
-              </div>
-          </div>
-          
-          <div className="p-4 flex flex-col justify-between">
-              <hr className="my-6 border-gray-200 dark:border-gray-400" />
-              
-              {/* Button for Room Settings */}
-              <RoomSettings roomName={props.roomName} isOwner={props.isRoomOwner} />
-              
-              <hr className="my-6 border-gray-200 dark:border-gray-400" />
-              
-              {/* Button for Sharing */}
-              <ShareRoom roomName={props.roomName} roomCode={props.id} />
-          </div>
+        </div>
       </div>
-    );
+
+      <div className="p-4 flex flex-col justify-between">
+        <hr className="my-6 border-gray-200 dark:border-gray-400" />
+
+        {/* Button for Room Settings */}
+        <RoomSettings roomName={props.roomName} isOwner={props.isRoomOwner} />
+
+        <hr className="my-6 border-gray-200 dark:border-gray-400" />
+
+        {/* Button for Sharing */}
+        <ShareRoom roomName={props.roomName} roomCode={props.roomCode} />
+      </div>
+    </div>
+  );
 };
 
 export default RightSidebar;
