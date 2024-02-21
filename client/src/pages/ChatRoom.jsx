79697d6f810996c4eea854323043a4d9854b5615
adrian.llonor <adrian.llonor@cs.ucalgary.ca>
2024-02-20 22:28:21 -0700

    Fixed div overflow with many rooms and members

--- client/src/pages/ChatRoom.jsx
+++ client/src/pages/ChatRoom.jsx
@@ -19,57 +19,64 @@ const ChatRoom = () => {
     <div className="container-center flex-row justify-between">
       {/* Left Sidebar */}
       <div className="flex flex-col justify-between w-1/5 min-w-fit h-full">
-        <div className="p-4 text-gray-800 h-[77%]">
-          <h1 className="text-lg font-bold mb-4 text-center">
-            Available Rooms
-          </h1>
+        <h1 className="text-lg font-bold mt-4 text-center">Available Rooms</h1>
+        <div className="p-4 text-gray-800 h-[70%] overflow-y-hidden">
 
-          {/* Scrollable Container for Room Components */}
-          <div
-            id="roomContainer"
-            className="overflow-y-auto flex flex-col items-center items-stretch space-y-3 h-full"
-          >
-            {/* Example for how Room components are used */}
-            <Room name="CPSC 559 Study Group" link="/chatroom" />
-            <Room name="413 Study Pals" />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
-            <Room />
+                {/* Scrollable Container for Room Components */}
+                <div id="roomContainer" className="overflow-y-auto flex flex-col items-center items-stretch space-y-3 h-full">
+                  {/* Example for how Room components are used */}
+                    <Room name="CPSC 559 Study Group" link="/chatroom"/>
+                    <Room name="413 Study Pals"/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room/>
+                    <Room name="Last Room 1"/>
+                    <Room name="Last Room 2"/>
+                    
+                    
+                  
+                </div>
           </div>
-        </div>
-        <div className="p-4 flex flex-col justify-between">
-          <hr className="my-6 border-gray-200 dark:border-gray-400" />
-
-          {/* Button for User Settings */}
-          <UserSettings />
-
-          <hr className="my-6 border-gray-200 dark:border-gray-400" />
-
-          {/* Container for Join Room and Create Room buttons */}
-          <div className="flex flex-col gap-4">
-            <JoinRoom />
-            <CreateRoom />
+          <div className="p-4 flex flex-col justify-between">
+            <hr className="my-6 border-gray-200 dark:border-gray-400" />
+              
+            {/* Button for User Settings */}
+            <Button variant="outlined" color="inherit" startIcon={<ManageAccountsIcon/>} className="flex-grow">
+                User Settings
+            </Button>
+            
+            <hr className="my-6 border-gray-200 dark:border-gray-400" />
+            
+            {/* Container for Join Room and Create Room buttons */}
+            <div className="flex flex-col gap-4">
+                {/* Button for Joining a Room */}
+                <Button variant="contained" startIcon={<GroupAddRoundedIcon />}>
+                    Join Room
+                </Button>
+                
+                {/* Button for Creating a Room */}
+                <Button variant="contained" color="secondary" startIcon={<AddRoundedIcon />}>
+                    Create Room
+                </Button>
+            </div>
           </div>
-        </div>
       </div>
 
       <Divider orientation="vertical" flexItem />
@@ -88,60 +95,61 @@ const ChatRoom = () => {
       {/* TODO: Spacing for both sidebars*/}
       {/* Right Sidebar */}
       <div className="flex flex-col justify-between h-full w-1/5 min-w-fit">
-        <div className="p-4 text-gray-900 h-[82%]">
-          <h1 className="text-lg mb-4 text-center">Current Members</h1>
-
-          {/* Scrollable Container for People Components */}
-          <div
-            id="memberContainer"
-            className="overflow-y-auto flex flex-col items-center items-stretch space-y-3 h-full"
-          >
-            {/* Example member components. Members should be dynamically added from the backend */}
-            <Member name="Patrick Bateman" isOwner="true" />
-            <Member name="Andrew 'Spider-Man' Garfield" isOwner="false" />
-            <Member name="Jennifer Lawrence" isOwner="false" />
-            <Member isAFK="true" />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
-            <Member />
+        <h1 className="text-lg mt-4 text-center">Current Members</h1>
+        <div className="p-4 text-gray-900 h-[82%] overflow-y-hidden">
+              
+              {/* Scrollable Container for People Components */}
+              <div id="memberContainer" className="overflow-y-auto flex flex-col items-center items-stretch space-y-3 h-full">
+                  {/* Example member components. Members should be dynamically added from the backend */}
+                  <Member name="Patrick Bateman" isOwner="true"/>
+                  <Member name="Andrew 'Spider-Man' Garfield" isOwner="false"/>
+                  <Member name="Jennifer Lawrence" isOwner="false"/>
+                  <Member isAFK="true"/>
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member />
+                  <Member name="Last Dude 1"/>
+                  <Member name="Last Dude 2"/>
+              </div>
           </div>
-        </div>
-
-        <div className="p-4 flex flex-col justify-between">
-          <hr className="my-6 border-gray-200 dark:border-gray-400" />
-
-          {/* Button for Room Settings */}
-          <RoomSettings roomName={roomName} isOwner={isRoomOwner} />
-
-          <hr className="my-6 border-gray-200 dark:border-gray-400" />
-
-          {/* Button for Sharing */}
-          <ShareRoom roomName={roomName} roomCode={id} />
-        </div>
+          
+          <div className="p-4 flex flex-col justify-between">
+              <hr className="my-6 border-gray-200 dark:border-gray-400" />
+              
+              {/* Button for Room Settings */}
+              <Button variant="outlined" color="inherit" startIcon={<RoomPreferencesIcon />} className="flex-grow">
+                  Room Settings
+              </Button>
+              
+              <hr className="my-6 border-gray-200 dark:border-gray-400" />
+              
+              {/* Button for Sharing */}
+              <Button variant="contained" color="success" endIcon={<ShareIcon />} className="flex-grow">
+                  Share
+              </Button>
+          </div>
       </div>
     </div>
   );
