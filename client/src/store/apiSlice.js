940ee35aedca4ec92d0038df810deec951f1f509
Ariel Lin <72408793+ariellinfy@users.noreply.github.com>
2024-03-12 20:13:26 -0600

    Fixed request api url confusion

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -1,7 +1,10 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000",
+    credentials: "include",
+  }),
   tagTypes: ["User", "Room", "Message"],
   endpoints: (builder) => ({}),
   refetchOnReconnect: true,
