516048857a759a14a599c961bf611036bf70a94b
Yvonne Ng <yvonneng0405@gmail.com>
2024-03-26 00:16:24 -0600

    Fixed wording in server logs

--- server/server.js
+++ server/server.js
@@ -61,5 +61,5 @@ app.get("/", (req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log("Server is running at:" `${process.env.CLIENT_URL}:${process.env.SERVER_URL}`);
+  console.log("Server is running at:" `${process.env.SERVER_URL}:${process.env.PORT}`);
 });
